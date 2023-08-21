3bb09f6068a99c30ee36659012aa1051825dc64a
ramenation <93734735+ramenation@users.noreply.github.com>
2023-08-21 18:08:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const video = document.getElementById('myVideo');
 const clickableArea = document.getElementById('clickableArea');
 
 video.addEventListener('timeupdate', function() {
-    if (video.currentTime >= 10 && video.currentTime <= 12) {  // Show clickable area between 10 to 12 seconds
+    if (video.currentTime >= 3 && video.currentTime <= 5) {  // Show clickable area between 10 to 12 seconds
         clickableArea.style.display = 'block';
     } else {
         clickableArea.style.display = 'none';
